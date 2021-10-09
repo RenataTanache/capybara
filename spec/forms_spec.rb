ae8f5e3bff28a4837840750be86984c303e3c517
rafaelmdsa <rafael.maciel@bowe.co>
2021-10-09 12:57:34 -0300

    estudo capybara teste de formulário

--- spec/forms_spec.rb
+++ spec/forms_spec.rb
@@ -1,12 +1,13 @@
+describe "Forms" do
+  it "Login com sucesso" do
+    visit "https://training-wheels-protocol.herokuapp.com/login"
 
-describe 'Forms' do
-is 'login com sucesso' do
-visit "https://training-wheels-protocol.herokuapp.com/login"
+    fill_in "username", with: "stark"
+    fill_in "password", with: "jarvis!"
+    click_button "Login"
+    expect(find("#flash").visible?).to be true
+    expect(find("#flash").text).to include "Olá, Tony Stark. Você acessou a área logada!"
 
-fill_in 'username', with: 'admin'
-fill_in 'password', with: 'admin'
-click_button 'login'
-
-
-     end
+    expect(find("#flash")).to have_content "Olá, Tony Stark. Você acessou a área logada!"
+  end
 end
